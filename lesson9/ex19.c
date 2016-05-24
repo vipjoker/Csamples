345511965aae76326ba00b953119d243f0208f77
Oleh Makhobei <tajcig@ya.ru>
2016-05-24 22:43:28 +0300

    Added Game classes

---
+++ lesson9/ex19.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "ex19.h"
+
+int Monster_attack(void *self, int damage)
+{
+	Monster *monster = self;
+	printf("You attack %s! \n",monster->_(description));
+
+	monster->hit_points -= damage;
+
+	if(monster->hit_points > 0){
+		printf("It is still alive.\n");
+		return 0;
+	}else {
+		printf("It is dead!\n");
+		return 1;
+	}
+}
+
+int Monster_init(void *self){
+	Monster *monster = self;
+	monster->hit_points = 10;
+	return 1;
+}
+
+Object MonsterProto = {
+	.init = Monster_init,
+	.attack = Monster_attack
+};
+
+void *Room_move (void *self, Direction direction){
+	Room *room = self;
+	Room *next = NULL;
+
+	if(direction == NORTH && room->north){
+		printf("You go north, into:\n");
+		next = room->north;
+	}
