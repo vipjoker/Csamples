db07b756b5bbbd4170a5c3d0ca02e702606e5444
omakhobei <tajcig@ya.ru>
2016-05-25 17:46:36 +0300

    added methods to ex.c

--- lesson9/ex19.c
+++ lesson9/ex19.c
@@ -39,4 +39,36 @@ void *Room_move (void *self, Direction direction){
 	if(direction == NORTH && room->north){
 		printf("You go north, into:\n");
 		next = room->north;
+	}else if (direction == SOUTH && room->south){
+		printf("You go south, into:\n");
+		next = room->south;
+	}else if (direction == EAST && room->east){
+		printf("You go east, into:  \n");
+		next = room->east;
+	}else if (direction == WEST && room->west){
+		printf("You go west, into:\n");
+		next = room->west;
+	}else {
+		printf("You can't go that direction");
+		next = NULL;
 	}
+	
+	if(next){
+		next->_(describe)(next);
+	}
+	return next;
+}
+
+int RoomAttack(void *self, int damage){
+
+	Room *room = self;
+	Monster *monster = room->bad_guy;
+
+	if(monster){
+		monster->_(attack)(monster,damage);
+		return 1;
+	}else{
+		printf("You flail in the air at nothing. Idiot.\n");
+		return 0;
+	}
+}
